d89b0f6bc1922fdc726ede40710b4e4b44847e11
Tetiana Tymoshchuk <teti.tym@gmail.com>
2022-04-22 21:21:45 +0200

    functions

--- src/com/itbulls/learnit/javacore/jcf/maps/TreeMapDemo.java
+++ src/com/itbulls/learnit/javacore/jcf/maps/TreeMapDemo.java
@@ -14,17 +14,17 @@ public class TreeMapDemo {
 
 	public static void main(String[] args) {
 		NavigableMap<Integer, String> treeMap = new TreeMap<>();
-		treeMap.put(1, "one");
+		treeMap.put(6, "six");
 		treeMap.put(5, "five");
 		treeMap.put(2, "two");
 		treeMap.put(4, "four");
 		treeMap.put(3, "three");
 		
 		System.out.println("Get first entry: " + treeMap.firstEntry());
-		System.out.println("Lower entry for 3: " + treeMap.lowerEntry(3));
+		System.out.println("Lower entry for 6: " + treeMap.lowerEntry(6));
 		System.out.println("Floor entry for 3: " + treeMap.floorEntry(3));
-		System.out.println("Higher entry for 3: " + treeMap.higherEntry(3));
-		System.out.println("Ceiling entry for 3: " + treeMap.ceilingEntry(3));
+		System.out.println("Higher entry for 5: " + treeMap.higherEntry(5));
+		System.out.println("Ceiling entry for 1: " + treeMap.ceilingEntry(1));
 		
 		System.out.println("Sorted treeMap: " + treeMap);
 		System.out.println("Descending order: " + treeMap.descendingMap());
